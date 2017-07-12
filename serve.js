3ad409af07208397a7736966b3544a256e12c558
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2017-07-13 00:44:54 +0300

    added heroku host if NODE_ENV == 'production'

--- serve.js
+++ serve.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var http = require('http');
+var host = '127.0.0.1';
+if(process.env.NODE_ENV == 'production') {
+    host = 'afternoon-everglades-97004.herokuapp.com';
+}
 
 http.createServer(function(request, response) {
     response.writeHead(200);
@@ -8,6 +12,6 @@ http.createServer(function(request, response) {
     response.end();
 })
 
-.listen(80, '127.0.0.1');
+.listen(80, host);
 
 console.log("Supah fly bot listening...");
