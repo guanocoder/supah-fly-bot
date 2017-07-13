b34e7cad8a2d0648847ebdcad2884dc067c9393d
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2017-07-14 01:38:41 +0300

    setting up bot webhook listener

--- serve.js
+++ serve.js
@@ -3,10 +3,28 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = 8080;
-if(process.env.NODE_ENV == 'production') {
-    port = process.env.PORT;
-}
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var port = process.env.PORT || 8080;
+var botToken = process.env.BOT_TOKEN || "AbcEEE";
+
+var updates = [];
+
+app.post(`/${botToken}`, (request, response) => {
+    updates.push({
+        date: new Date(),
+        query: request.query,
+        params: request.params,
+        body: request.body
+    });
+    response.end();    
+});
+
+app.get('/getUpdates', (request, response) => {
+    response.json(updates);
+});
 
 app.use(express.static('www'));
 app.get('*', (request, response) => {
