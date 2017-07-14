772dae436067e4694646db66ebb58e6a4f203ac3
guanocoder <spy.white@mail.ru>
2017-07-14 20:46:14 +0300

    simple reply any message

--- serve.js
+++ serve.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var requestModule = require('request');
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -15,6 +16,22 @@ var updates = [];
 app.post(`/${botToken}`, (request, response) => {
     updates.push(request.body);
     response.end();
+    if(request.body.message) {
+        setTimeout(() => {
+            requestModule.post({
+                url: `https://api.telegram.org/bot${botToken}/sendMessage`,
+                form: {
+                    chat_id : request.body.message.chat.id,
+                    text: "заткнись!",
+                    reply_to_message_id: request.body.message.message_id
+                }
+            }, (error, response, body) => {
+                if(error) {
+                    console.log("Error: " + error);
+                }
+            });
+        }, 1000 + Math.round(Math.random()*2000));
+    }
 });
 
 app.get('/getUpdates', (request, response) => {
