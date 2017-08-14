70ff223e6631a323a6785fc4213ea7c39db59f6d
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2017-08-14 23:35:08 +0300

    added "InputTextMessageContent" to answerInlineQuery method to see if it works

--- bot/message-handlers/inline-query-handler.js
+++ bot/message-handlers/inline-query-handler.js
@@ -18,7 +18,10 @@ InlineQueryHandler.prototype.handle = function(update) {
                     type: "sticker",
                     id: 10000000 + parseInt(Math.random() * 10000000),
                     //sticker_file_id: "AAQCABMmREsNAAShSvrOvhTY9aZdAAIC" // thumb.file_id
-                    sticker_file_id: "CAADAgADqQADWQMDAAE8ExBJs_WvHAI"
+                    sticker_file_id: "CAADAgADqQADWQMDAAE8ExBJs_WvHAI",
+                    input_message_content: {
+                        message_text: "And nothin!"
+                    }
                 }
             ])
         );
