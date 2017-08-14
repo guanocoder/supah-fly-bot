4a563d9b39634c74f8f3757efccf236f49456ffe
guanocoder <spy.white@mail.ru>
2017-08-14 18:51:11 +0300

    trying to return results to inline query mode 5

--- bot/message-handlers/inline-query-handler.js
+++ bot/message-handlers/inline-query-handler.js
@@ -13,14 +13,14 @@ InlineQueryHandler.prototype.canHandle = function(update) {
 InlineQueryHandler.prototype.handle = function(update) {
     return new Promise((resolve, reject) => {
         resolve(
-            telegram.answerInlineQuery(update.inline_query.id,
+            telegram.answerInlineQuery(update.inline_query.id, [
                 {
                     type: "sticker",
                     id: 10000000 + parseInt(Math.random() * 10000000),
                     //sticker_file_id: "AAQCABMmREsNAAShSvrOvhTY9aZdAAIC" // thumb.file_id
                     sticker_file_id: "CAADAgADqQADWQMDAAE8ExBJs_WvHAI"
                 }
-            )
+            ])
         );
     });
 };
