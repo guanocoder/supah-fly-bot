6a2632c9124abb341b143891afd21e09375a3ad8
mikser.vladimirovich <mikser.vladimirovich@yandex.ru>
2017-08-16 01:46:36 +0300

    inline choices configured by keywords in a separate file

---
+++ bot/message-handlers/inline-dictionary.js
@@ -0,0 +1,82 @@
+module.exports = {
+    "молчи": "stfu",
+    "заткнись": "stfu",
+    "shutup": "stfu",
+    "shut up": "stfu",
+    "stfu": [
+        {
+            type: "sticker",
+            file_id: "CAADAgADqQADWQMDAAE8ExBJs_WvHAI", // заткнись, я воевал!
+        },
+        {
+            type: "sticker",
+            file_id: "CAADAgADtgADNPsXBpaFuBVOTp4lAg" // anime - молчи г*вно
+        },
+        {
+            type: "sticker",
+            file_id: "CAADAgADVAIAAjT7FwZf3h1dJFbWtwI" // anime - завали уже!
+        },
+        {
+            type: "sticker",
+            file_id: "CAADAgADNgADaJpdDMPkFTIssktrAg" // та замовкнi тi вже!
+        },
+        {
+            type: "sticker",
+            file_id: "CAADAgADSgEAAjeEMAABYXK6nmHQkkAC" // troll.uz - заткниииись!
+        },
+        {
+            type: "sticker",
+            file_id: "CAADAgADkAADaJpdDIc4tgssUVXpAg" // а что если завалишь?
+        },
+        {
+            type: "mpeg4_gif",
+            file_id: "CgADBAADNSEAAs0aZAfUl1x1IJllcAI" // stfu pitch!
+        }
+    ],
+    "goldfish": "blah",
+    "blah blah": "blah",
+    "blah": [
+        {
+            type: "mpeg4_gif",
+            file_id: "CgADBAAD9J8AAkkcZAcHStrL64TypAI" // spongebob goldfishing
+        }
+    ],
+    "me no care": "no care",
+    "i dont care": "no care",
+    "i don't care": "no care",
+    "dont care": "no care", 
+    "don't care": "no care",
+    "no care": [
+        {
+            type: "sticker",
+            file_id: "CAADBAADaQEAAjW7NgABVwW8yS94zhYC" // spongebob - I don't give a funk
+        },
+        {
+            type: "mpeg4_gif",
+            file_id: "CgADBAADaZ8AAnQXZAeLI66l6MtbeAI" // Harrison Ford - who gives a puck?
+        }
+    ],
+    "иди вон": "gtfo",
+    "пшел вон": "gtfo",
+    "пшёл вон": "gtfo",
+    "пошёл вон": "gtfo",
+    "get out": "gtfo",
+    "gtfo": [
+        {
+            type: "sticker",
+            file_id: "CAADAgADFgEAAsnHuArAgVwG6bz5kwI" // жирик - пшёл вон!
+        }
+    ],
+    "нечего ответить": "nothing to say",
+    "не могу ответить": "nothing to say",
+    "can't answer": "nothing to say",
+    "cant answer": "nothing to say",
+    "can't say": "nothing to say",
+    "cant say": "nothing to say",
+    "nothing to say": [
+        {
+            type: "sticker",
+            file_id: "CAADAgADNwMAApkvSwrd8kC724H93QI" // дружко - не могу ответить!
+        }
+    ]
+};
--- bot/message-handlers/inline-query-handler.js
+++ bot/message-handlers/inline-query-handler.js
@@ -1,4 +1,5 @@
 var telegram = require("../../api/telegram");
+var inlineDictionary = require("./inline-dictionary");
 
 var InlineQueryHandler = function() {};
 
@@ -12,70 +13,41 @@ InlineQueryHandler.prototype.canHandle = function(update) {
 
 InlineQueryHandler.prototype.handle = function(update) {
     return new Promise((resolve, reject) => {
+        let inlineChoices = inlineDictionary[update.inline_query.query];
+
+        while(typeof(inlineChoices) == "string") {
+            inlineChoices = inlineDictionary[inlineChoices];
+        }
+
+        if(!Array.isArray(inlineChoices)) {
+            inlineDictionary =
+                inlineDictionary["stfu"]
+                .concat(inlineDictionary["blah"])
+                .concat(inlineDictionary["no care"])
+                .concat(inlineDictionary["gtfo"])
+                .concat(inlineDictionary["nothing to say"]);
+        }
+
+        inlineChoices.map((choiceItem) => {
+            chiceItem.id = String(10000000 + parseInt(Math.random() * 10000000));
+            switch(choiceItem.type) {
+                case "sticker":
+                    choiceItem.sticker_file_id = choiceItem.file_id;
+                    break;
+                case "mpeg4_gif":
+                    choiceItem.mpeg4_file_id = choiceItem.file_id;
+                    break;
+                default:
+                    return null;
+                    break;
+            }
+            delete choiceItem.file_id;
+            return choiceItem;
+        });
+        
         resolve(
             telegram.answerInlineQuery(update.inline_query.id, 
-                JSON.stringify([
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADqQADWQMDAAE8ExBJs_WvHAI", // заткнись, я воевал!
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADtgADNPsXBpaFuBVOTp4lAg" // anime - молчи г*вно
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADBAADaQEAAjW7NgABVwW8yS94zhYC" // spongebob - I don't give a funk
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADFgEAAsnHuArAgVwG6bz5kwI" // жирик - пшёл вон!
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADVAIAAjT7FwZf3h1dJFbWtwI" // anime - завали уже!
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADNwMAApkvSwrd8kC724H93QI" // дружко - не могу ответить!
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADNgADaJpdDMPkFTIssktrAg" // та замовкнi тi вже!
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADSgEAAjeEMAABYXK6nmHQkkAC" // troll.uz - заткниииись!
-                    },
-                    {
-                        type: "sticker",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        sticker_file_id: "CAADAgADkAADaJpdDIc4tgssUVXpAg" // а что если завалишь?
-                    },
-                    {
-                        type: "mpeg4_gif",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        mpeg4_file_id: "CgADBAADNSEAAs0aZAfUl1x1IJllcAI" // stfu pitch!
-                    },
-                    {
-                        type: "mpeg4_gif",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        mpeg4_file_id: "CgADBAAD9J8AAkkcZAcHStrL64TypAI" // spongebob goldfishing
-                    },
-                    {
-                        type: "mpeg4_gif",
-                        id: String(10000000 + parseInt(Math.random() * 10000000)),
-                        mpeg4_file_id: "CgADBAADaZ8AAnQXZAeLI66l6MtbeAI" // Harrison Ford - who gives a puck?
-                    }
-                ])
+                JSON.stringify(inlineChoices)
             )
         );
     });
