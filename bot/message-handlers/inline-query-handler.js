803c4be30e88b82dcae50475266963bdafdaaa0b
guanocoder <spy.white@mail.ru>
2017-08-18 11:47:10 +0300

    results should be now picked from the database

--- bot/message-handlers/inline-query-handler.js
+++ bot/message-handlers/inline-query-handler.js
@@ -1,4 +1,5 @@
 var telegram = require("../../api/telegram");
+var database = require("../../api/database");
 var inlineDictionary = require("./inline-dictionary");
 
 var InlineQueryHandler = function() {};
@@ -13,50 +14,45 @@ InlineQueryHandler.prototype.canHandle = function(update) {
 
 InlineQueryHandler.prototype.handle = function(update) {
     return new Promise((resolve, reject) => {
-        let inlineChoices = inlineDictionary[update.inline_query.query.toLowerCase()];
-
-        while(typeof(inlineChoices) == "string") {
-            inlineChoices = inlineDictionary[inlineChoices];
-        }
-
-        // return this bunch of choices if no match is found
-        if(!Array.isArray(inlineChoices)) {
-            inlineChoices =
-                inlineDictionary["stfu"]
-                .concat(inlineDictionary["blah"])
-                .concat(inlineDictionary["no care"])
-                .concat(inlineDictionary["gtfo"])
-                .concat(inlineDictionary["nothing to say"])
-                .concat(inlineDictionary["suck"]);
-        }
-
-        inlineChoices = inlineChoices.map((choiceItem) => {
-            let resultItem = {
-                type: choiceItem.type,
-                id: String(10000000 + parseInt(Math.random() * 10000000))
-            };
-            switch(choiceItem.type) {
-                case "sticker":
-                    resultItem.sticker_file_id = choiceItem.file_id;
-                    break;
-                case "mpeg4_gif":
-                    resultItem.mpeg4_file_id = choiceItem.file_id;
-                    break;
-                case "photo":
-                    resultItem.photo_file_id = choiceItem.file_id;
-                    break;
-                default:
-                    return null;
-                    break;
+        database.lookupResults[update.inline_query.query.toLowerCase()].then((results) => {
+            // return this bunch of choices if no match is found
+            if(!Array.isArray(results)) {
+                results =
+                    inlineDictionary["stfu"]
+                    .concat(inlineDictionary["blah"])
+                    .concat(inlineDictionary["no care"])
+                    .concat(inlineDictionary["gtfo"])
+                    .concat(inlineDictionary["nothing to say"])
+                    .concat(inlineDictionary["suck"]);
             }
-            return resultItem;
-        });
 
-        resolve(
-            telegram.answerInlineQuery(update.inline_query.id, 
-                JSON.stringify(inlineChoices)
-            )
-        );
+            let inlineChoices = results.map((choiceItem) => {
+                let resultItem = {
+                    type: choiceItem.type,
+                    id: String(10000000 + parseInt(Math.random() * 10000000))
+                };
+                switch(choiceItem.type) {
+                    case "sticker":
+                        resultItem.sticker_file_id = choiceItem.file_id;
+                        break;
+                    case "mpeg4_gif":
+                        resultItem.mpeg4_file_id = choiceItem.file_id;
+                        break;
+                    case "photo":
+                        resultItem.photo_file_id = choiceItem.file_id;
+                        break;
+                    default:
+                        return null;
+                        break;
+                }
+                return resultItem;
+            });
+
+            resolve(telegram.answerInlineQuery(update.inline_query.id, JSON.stringify(inlineChoices)));
+            
+        }).catch((error) => {
+            reject(error);
+        });
     });
 };
 
