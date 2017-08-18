773d4f03591a93caa57e9461dbe858860cfe34e8
guanocoder <spy.white@mail.ru>
2017-08-18 12:32:20 +0300

    ok now I really fixed it

--- bot/message-handlers/inline-query-handler.js
+++ bot/message-handlers/inline-query-handler.js
@@ -16,29 +16,7 @@ InlineQueryHandler.prototype.handle = function(update) {
     return new Promise((resolve, reject) => {
         database.lookupResults(update.inline_query.query.toLowerCase()).then((result) => {
             let inlineChoices = null;                        
-            if(result && Array.isArray(result.rows) && result.rows.length > 0) {
-                let inlineChoices = result.rows.map((row) => {
-                    let resultItem = {
-                        type: row.type,
-                        id: String(10000000 + parseInt(Math.random() * 10000000))
-                    };
-                    switch(row.type) {
-                        case "sticker":
-                            resultItem.sticker_file_id = row.file_id;
-                            break;
-                        case "mpeg4_gif":
-                            resultItem.mpeg4_file_id = row.file_id;
-                            break;
-                        case "photo":
-                            resultItem.photo_file_id = row.file_id;
-                            break;
-                        default:
-                            return null;
-                            break;
-                    }
-                    return resultItem;
-                });
-            } else {
+            if(result == null || !Array.isArray(result.rows) || result.rows.length == 0) {
                 // return this bunch of choices if no match is found
                 inlineChoices = inlineDictionary["stfu"]
                     .concat(inlineDictionary["blah"])
@@ -46,7 +24,31 @@ InlineQueryHandler.prototype.handle = function(update) {
                     .concat(inlineDictionary["gtfo"])
                     .concat(inlineDictionary["nothing to say"])
                     .concat(inlineDictionary["suck"]);
+            } else {
+                inlineChoices = result.rows;
             }
+
+            inlineChoices = inlineChoices.map((row) => {
+                let resultItem = {
+                    type: row.type,
+                    id: String(10000000 + parseInt(Math.random() * 10000000))
+                };
+                switch(row.type) {
+                    case "sticker":
+                        resultItem.sticker_file_id = row.file_id;
+                        break;
+                    case "mpeg4_gif":
+                        resultItem.mpeg4_file_id = row.file_id;
+                        break;
+                    case "photo":
+                        resultItem.photo_file_id = row.file_id;
+                        break;
+                    default:
+                        return null;
+                        break;
+                }
+                return resultItem;
+            });        
 
             resolve(telegram.answerInlineQuery(update.inline_query.id, JSON.stringify(inlineChoices)));
 
