304b833e548e0ae33670c4648976999539edaba3
guanocoder <spy.white@mail.ru>
2017-08-18 12:09:37 +0300

    fixed it

--- bot/message-handlers/inline-query-handler.js
+++ bot/message-handlers/inline-query-handler.js
@@ -14,39 +14,39 @@ InlineQueryHandler.prototype.canHandle = function(update) {
 
 InlineQueryHandler.prototype.handle = function(update) {
     return new Promise((resolve, reject) => {
-        database.lookupResults(update.inline_query.query.toLowerCase()).then((results) => {
-            // return this bunch of choices if no match is found
-            if(!Array.isArray(results)) {
-                results =
-                    inlineDictionary["stfu"]
+        database.lookupResults(update.inline_query.query.toLowerCase()).then((result) => {
+            let inlineChoices = null;                        
+            if(result && Array.isArray(result.rows) && result.rows.length > 0) {
+                let inlineChoices = result.rows.map((row) => {
+                    let resultItem = {
+                        type: row.type,
+                        id: String(10000000 + parseInt(Math.random() * 10000000))
+                    };
+                    switch(row.type) {
+                        case "sticker":
+                            resultItem.sticker_file_id = row.file_id;
+                            break;
+                        case "mpeg4_gif":
+                            resultItem.mpeg4_file_id = row.file_id;
+                            break;
+                        case "photo":
+                            resultItem.photo_file_id = row.file_id;
+                            break;
+                        default:
+                            return null;
+                            break;
+                    }
+                    return resultItem;
+                });
+            } else {
+                // return this bunch of choices if no match is found
+                inlineChoices = inlineDictionary["stfu"]
                     .concat(inlineDictionary["blah"])
                     .concat(inlineDictionary["no care"])
                     .concat(inlineDictionary["gtfo"])
                     .concat(inlineDictionary["nothing to say"])
                     .concat(inlineDictionary["suck"]);
             }
-
-            let inlineChoices = results.map((choiceItem) => {
-                let resultItem = {
-                    type: choiceItem.type,
-                    id: String(10000000 + parseInt(Math.random() * 10000000))
-                };
-                switch(choiceItem.type) {
-                    case "sticker":
-                        resultItem.sticker_file_id = choiceItem.file_id;
-                        break;
-                    case "mpeg4_gif":
-                        resultItem.mpeg4_file_id = choiceItem.file_id;
-                        break;
-                    case "photo":
-                        resultItem.photo_file_id = choiceItem.file_id;
-                        break;
-                    default:
-                        return null;
-                        break;
-                }
-                return resultItem;
-            });
 
             resolve(telegram.answerInlineQuery(update.inline_query.id, JSON.stringify(inlineChoices)));
 
