5b503e759b46be2e42bc81dc12ee26d40e1bac88
guanocoder <spy.white@mail.ru>
2017-09-19 01:13:38 +0300

    bug: forgot that results should be an array

--- bot/message-handlers/inline-query-render-handler.js
+++ bot/message-handlers/inline-query-render-handler.js
@@ -16,12 +16,12 @@ Handler.prototype.canHandle = function(update) {
 Handler.prototype.handle = function(update) {
     let photo_url = `http://afternoon-everglades-97004.herokuapp.com/render/kit/${encodeURI(update.inline_query.query)}`;
     return telegram.answerInlineQuery(update.inline_query.id, JSON.stringify(
-        {
+        [{
             type: "photo",
             id: String(10000000 + parseInt(Math.random() * 10000000)),
             photo_url: photo_url,
             thumb_url: photo_url
-        }
+        }]
     ));
 };
 
