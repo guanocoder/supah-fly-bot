e2c5831e53ddf39bee22bd8ed61a4481656c9d9c
guanocoder <spy.white@mail.ru>
2017-12-26 01:53:24 +0300

    small fix in the exec-handler

--- bot/message-handlers/exec-handler.js
+++ bot/message-handlers/exec-handler.js
@@ -23,6 +23,7 @@ Handler.prototype.canHandle = function(update) {
 Handler.prototype.handle = function(update) {
 
     let message = update.message || update.edited_message;
+    let __anyMessageSent = false;
     
     function TelegramConsoleImitation() {
         let loggedLines = [];
@@ -40,6 +41,7 @@ Handler.prototype.handle = function(update) {
     }
 
     function sendMessage(messageText) {
+        __anyMessageSent = true;
         return telegram.sendMessage(message.chat.id, messageText);
     }
     
@@ -49,12 +51,20 @@ Handler.prototype.handle = function(update) {
     try {
         __result = String(eval(message.text.substring(5)));
     } catch(error) {
-        __result = String(error);
+        console.log(String(error));
     }
-    __result = `<code>${__result}</code>`;
+
+    if(!console.anythingLogged()) {
+        if(__anyMessageSent) {
+            return Promise.resolve(); // no need to respond then
+        } else {
+            console.log(__result);
+        }
+    }
+
     return telegram.sendMessage(
             message.chat.id,
-            (console.anythingLogged()) ? console.toString() : __result,
+            console.toString(),
             (update.edited_message) ? update.edited_message.message_id : undefined, undefined, "HTML"
         ).catch(error => {
             this.__systemConsole.log("Error: could not send /exec response via telegram sendMessage() - " + error)
